d893c9815ca8f6e52ec212340dab7062a2eaf575
Emmanuel Awoyemi <150502155+EAwoyemi110@users.noreply.github.com>
2026-03-26 12:31:00 -0500

    Add files via upload

---
+++ QR qode generator/main.py
@@ -0,0 +1,23 @@
+import pyqrcode
+import png
+from pyqrcode import QRCode
+import tkinter as tk
+from tkinter import *
+
+'''
+Tkinter Module – helps to create the GUI window for the project.
+Png Module – This helps to save an image in the png extension.
+Pyqrcode Module – This Module helps to generate a QR Code.
+'''
+#---Create a window
+window = Tk()
+
+window.geometry('400x450')
+
+window.title('PythonGeeks')
+
+Label(window, text="Let's make a qr code!" font='arial 15').pack()
+
+
+#---Create a window
+
